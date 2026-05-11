123ccd5c5568885e27508e8b35dafbd8a3065d31
Nkeem <dk4922905@gmail.com>
2026-05-11 23:17:47 +0300

    bind request parameters explicitly

--- core-service/src/main/java/com/bank/core/controller/TransferController.java
+++ core-service/src/main/java/com/bank/core/controller/TransferController.java
@@ -37,7 +37,7 @@ public class TransferController {
 
     @GetMapping("/history")
     @PreAuthorize("isAuthenticated()")
-    public UniversalResponse<List<TransactionDto>> getHistory(@RequestParam Long accountId) {
+    public UniversalResponse<List<TransactionDto>> getHistory(@RequestParam("accountId") Long accountId) {
         log.info("Request to get transfer history by accountId: {}", accountId);
         UUID userId = SecurityUtils.getCurrentUserId();
         return transferService.getHistory(accountId, userId);
